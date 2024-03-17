8dfd8f900435bfc87835545658c56ea1861ef94b
ferdinan04 <ferdinan.rebaza@pucp.pe>
2024-03-16 19:00:26 -0500

    update of url in data

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://cnt-7d0b3ca1-8ebb-455c-9c3e-75cea65d9282.containerhub.tripleten-services.com?lng=es'
+urban_routes_url = 'https://cnt-83a15c29-c15c-4c7f-b5ff-46d13fa790b2.containerhub.tripleten-services.com?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
